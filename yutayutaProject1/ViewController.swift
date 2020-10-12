feb2e8310206fd356a74347cdefd9200d3ea55a5
taka <tomoko.takahashi715@gmail.com>
2020-10-12 15:31:07 +0900

    Initial Commit

---
+++ yutayutaProject1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  yutayutaProject1
+//
+//  Created by Takahashi Tomoko on 2020/10/12.
+//  Copyright © 2020 takatomo.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
